7a9508d9b68b568afde7725fcdf4f13723881122
RaeBaek <bmp117@naver.com>
2025-12-10 21:39:52 +0900

    test: TwoCountersFeature 테스트 구현
    - 기존 비어 있던 테스트 제거 후 XCTest 기반 단위 테스트 작성
    - counter1 / counter2 각각의 incrementDebounced 동작 검증
    - resetAll 실행 시 두 카운터의 State 초기화 여부 확인
    - TestStore 기반으로 Reducer 스코프 동작 및 자식 리듀서 연동 검증

--- Features/TwoCounters/Tests/TwoCounters.swift
+++ Features/TwoCounters/Tests/TwoCounters.swift
@@ -1,5 +1,44 @@
-import Foundation
+//
+//  CounterFeature.swift
+//  Counter
+//
+//  Created by 백래훈 on 12/5/25.
+//
 
-public struct TwoCounters {
-    public init() {}
+import XCTest
+import ComposableArchitecture
+@testable import Counter
+@testable import TwoCounters
+
+final class TwoCountersFeatureTests: XCTestCase {
+    func test_ResetAll() async {
+        let store = await TestStore(initialState: TwoCountersFeature.State()) {
+            TwoCountersFeature()
+        }
+
+        await store.send(.counter1(.incrementDebounced)) {
+            $0.counter1.count = $0.counter1.stepValue
+        }
+
+        await store.send(.counter2(.incrementDebounced)) {
+            $0.counter2.count = $0.counter2.stepValue
+        }
+
+        let counter1Value = await store.state.counter1.count
+        let counter2Value = await store.state.counter2.count
+
+        XCTAssertEqual(counter1Value, 1)
+        XCTAssertEqual(counter2Value, 1)
+
+        await store.send(.resetAll) {
+            $0.counter1.count = 0
+            $0.counter2.count = 0
+        }
+
+        let counter1Reset = await store.state.counter1.count
+        let counter2Reset = await store.state.counter2.count
+
+        XCTAssertEqual(counter1Reset, 0)
+        XCTAssertEqual(counter2Reset, 0)
+    }
 }
